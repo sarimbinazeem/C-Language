37efde549992eb80bec73a667219c41d65f947d1
Sarim Bin Azeem <sarimbinaz@gmail.com>
2026-01-12 15:21:49 +0500

    Fitness progress tracker updated

--- Functions/FitnessTracker.c
+++ Functions/FitnessTracker.c
@@ -1,25 +1,54 @@
-// Write a C program that tracks a person’s daily calorie intake over a week using arrays and functions. The program should prompt the user to enter the number of calories consumed each day for seven days, ensuring that all inputs are non-negative values.
+// Write a C program that tracks a user’s weekly fitness progress using arrays and functions. The program should record daily calorie burn for a week (7 days), validate that all inputs are non-negative, and allow the user to repeat the process for multiple weeks.
 
-// After collecting the data, calculate the total weekly calorie intake and compute the average calories consumed per day. The program should demonstrate the use of one-dimensional arrays, input validation, user-defined functions, and basic arithmetic operations to analyze fitness progress.
+// For each week, the program should:
 
+// Take daily calorie input for all 7 days,
+
+// Calculate the total and average calories burned,
+
+// Determine the most active day (the day with the highest calories),
+
+// Provide feedback based on the average calories burned,
+
+// Display a detailed weekly summary including daily calories, total calories, average calories, and the most active day.
+
+// The program should be menu- or loop-driven to allow tracking progress across multiple weeks until the user chooses to exit, demonstrating the use of arrays, loops, conditional statements, and user-defined functions.
 
 #include <stdio.h>
 #define WEEK 7
 
 void inputCalories(int calories[WEEK]);
+int totalCalories(int calories[WEEK]);
 float averageCalories(int total);
+int activeDay(int calories[WEEK]);
+void feedback(float average);
+void displaySummary(int calories[WEEK], int total, float average, int active);
 
 int main()
 {
-	int calories[WEEK],total;
+	int calories[WEEK],total, active,i=1,choice;
 	float average;
 	
-	printf("\n\n==========Fitness Progress Tracker==========\n");
+	do
+	{
+    printf("\n\n==========Fitness Progress Tracker==========\n");
+	printf("\n-----Week %d-----\n",i);
 	inputCalories(calories);
 	
 	total = totalCalories(calories);
 	average = averageCalories(total);
+	active = activeDay(calories);
+	feedback(average);
+	displaySummary(calories,total,average,active);
 	
+	printf("Do You Want To Proceed To Next Week?(1 for Yes, 0 for No): ");
+	scanf("%d",&choice);
+	i++;
+	}while(choice !=0);
+	
+	printf("Exiting The Program... \n");
+	
+	return 0;
 }
 
 void inputCalories(int calories[WEEK])
@@ -54,3 +83,51 @@ float averageCalories(int total)
 	return avg;
 }
 
+int activeDay(int calories[WEEK])
+{
+    int max, day =1;
+    max = calories[0];
+    for(int i=1;i<WEEK;i++)
+    {
+        if(calories[i]>max)
+        {
+            max = calories[i];
+            day = i+1;
+        }
+    }
+    
+    return day;
+}
+
+void feedback(float average)
+{
+    printf("Your Average Is: %.2f\n",average);
+    if(average>=750)
+    {
+        printf("Excellent Progress! \n");
+    }
+    else if(average>=500)
+    {
+        printf("Good Effort! \n");
+    }
+    else
+    {
+        printf("Needs Improvement! \n");
+    }
+    
+}
+
+void displaySummary(int calories[WEEK], int total, float average, int active)
+{
+    printf("\n==========Fitness Summary==========\n\n");
+    
+    printf("%-10s %-10s \n","Day","Calories");
+    for(int i=0;i<WEEK;i++)
+    {
+        printf("%-10d %-10d \n",i+1,calories[i]);
+    }
+    
+    printf("Total Calories: %d\n",total);
+    printf("Average Calories: %.2f\n",average);
+    printf("Most Active Day: %d\n",active);
+}
